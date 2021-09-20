effac9dd4907783d99ac5d84ed26a6fda495ca66
biareiam <biareiam@gmail.com>
2021-09-20 15:20:34 +0000

    refactoring sles and surplus update worksheet

--- run.py
+++ run.py
@@ -82,15 +82,46 @@ def validate_data(values):
 
    return True
 
-
-def update_sales_worksheet(data):
+   """
+   This two function do the same thing, so we will refactor it.
+   refactoring is the restructuring of the code to improve its operation, without altering
+   its functionality
+   """
+   """
+   def update_sales_worksheet(data):
+   """
    """
    Update sales worksheet, add new row with the list data provided.
+   """
    """
    print("Updating sales wroksheet...\n")   
    sales_worksheet = SHEET.worksheet("sales")
    sales_worksheet.append_row(data)
    print("Sales worksheet updated successfuly.\n")
+
+
+def update_surplus_worksheet(data):
+   """
+   """
+   Update surplus worksheet, add new row with the list data provided.
+   """
+   """
+   print("Updating sales wroksheet...\n")   
+   surplus_worksheet = SHEET.worksheet("surplus")
+   surplus_worksheet.append_row(data)
+   print("Surplus worksheet updated successfuly.\n")
+   """
+
+def update_worksheet(data,worksheet):
+   """
+   Receives a list of integers to be inserted into a worksheet
+   Update the relevant worksheet with the data provided.
+   """
+   print(f"Updating {worksheet} wroksheet...\n")   
+   worksheet_to_update = SHEET.worksheet(worksheet)
+   worksheet_to_update.append_row(data)
+   print(f"{worksheet} worksheet updated successfully.\n")
+
 
 
 
@@ -108,6 +139,10 @@ def calculate_surplus_data(sales_row):
    
 
    surplus_data = [] 
+   """ 
+   The zip code is used to iterate 2 lists at the same time
+   """
+
    for stock, sales in zip(stock_row,sales_row):
       surplus = int(stock) - sales
       surplus_data.append(surplus)
@@ -123,9 +158,16 @@ def main():
    """
    data = get_sales_data()
    sales_data = [int(num) for num in data]
+   """
    update_sales_worksheet(sales_data)
+   """
+   update_worksheet(sales_data,"sales")
    new_surplus_data = calculate_surplus_data(sales_data)
-   print(new_surplus_data)
+   """
+   update_surplus_worksheet(new_surplus_data)
+   """
+   update_worksheet(new_surplus_data, "surplus")
+
    
 
 
